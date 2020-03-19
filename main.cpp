824da51c7810ecb89ef47f733bfdd7c37d18b5ef
lera <bryzgalovalera@mail.ru>
2020-03-20 01:24:20 +0300

    main complement1

--- main.cpp
+++ main.cpp
@@ -38,8 +38,36 @@ void lose(RenderWindow & window)
 
 int main()
 {
+	Map drawing;
+	Player z("cat.png", 78, 48, 20.0, 20.0); 
+	Map m;
+	Clock clock;
 
 
+	std::list<Entity*> enemy;//список врагов
+	std::list<Entity*>::iterator ee;
+
+
+	std::list<Push*> pus;//распылители
+	std::list<Push*>::iterator pp;
+
+	std::list<Push::Bullet*> bullets;//пули(капли)
+	std::list<Push::Bullet*>::iterator bul;
+//
+
+	Font font;
+	font.loadFromFile("Ben_Krush.ttf");
+	Text text("", font, 20);
+	text.setStyle(sf::Text::Bold);
+//
+
+	text.setString("«доровье:");
+	text.setPosition(400, 0);
+	window.draw(text);
+	window.draw(z.hpbar);
+	window.display();
+	
+	
 	return 0;
 }
 
